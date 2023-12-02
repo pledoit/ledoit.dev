f5fc70c19b1d79f510055eac9fdcced2821da50e
Phil Ledoit <pledoit@bu.edu>
2023-12-01 22:25:29 -0500

    express ts backbone

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server WE OUT HERE AND ANOTHER EDIT AND A FINAL ONE? no way we can do another one... but we can!');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
